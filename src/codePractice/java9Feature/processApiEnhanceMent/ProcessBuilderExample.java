409aa5e4424ae409e54265f82398b864213a61b1
akash <ajind033@gmail.com>
2023-07-10 23:39:07 +0530

    Java 9 feature | Process Builder Ex Update

--- src/codePractice/java9Feature/processApiEnhanceMent/ProcessBuilderExample.java
+++ src/codePractice/java9Feature/processApiEnhanceMent/ProcessBuilderExample.java
@@ -3,7 +3,10 @@ package codePractice.java9Feature.processApiEnhanceMent;
 public class ProcessBuilderExample {
 
     public static void main(String[] args) throws Exception {
-        new ProcessBuilder("jshell").inheritIO().start().waitFor();
+    	ProcessBuilder  pb =  new ProcessBuilder("jshell");
+    	Process p =pb.inheritIO().start();
+    	Thread.sleep(5_000l);
+    	p.destroy();
         System.out.println("ProcessBuilderExample exiting");
     }
 }
